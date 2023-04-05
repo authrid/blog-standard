20923bd0c6c3cc7fd06aef4129b9165594ea227f
authrid <power.of.author@gmail.com>
2023-04-06 05:27:30 +0700

    set Auth0 file login

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>
-    <h1>This is the homepage</h1>
-  </div>;
+  return (
+    <div>
+      <h1>This is the homepage</h1>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
+    </div>
+  )
 }
